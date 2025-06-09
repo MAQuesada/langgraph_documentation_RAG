8c0e6af9fc9e7735072ce2c5016da17dce6374a8
Utkarsh Dubey <utkarsh251096@gmail.com>
2025-06-09 22:47:52 +0530

    text_splitter populated

--- vector_database/src/text_splitter.py
+++ vector_database/src/text_splitter.py
@@ -1,6 +1,23 @@
-from dotenv import load_dotenv
-import os
+from pathlib import Path
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-load_dotenv()
+def split_text_files(input_dir, chunk_size=500, chunk_overlap=50):
+    input_dir = Path(input_dir)
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap
+        )
+    
+    chunks_with_metadata = []
 
-print("OpenAI Key:", os.getenv("OPENAI_API_KEY"))
+    for txt_file in input_dir.glob('*.txt'):
+        with open(txt_file, 'r', encoding='utf-8') as f:
+            content = f.read()
+
+        splits = splitter.split_text(content)
+        chunks_with_metadata.extend([{
+            "content" : chunk,
+            "source" : txt_file.name
+        } for chunk in splits])
+
+        return chunks_with_metadata
